105d7b5f0f9ef19b3885cf8fdc0bdf77b3363585
Hanna Tubolets <anna.tubolets@gmail.com>
2023-06-29 14:33:16 +0300

    Updated files

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -25,31 +25,3 @@ export async function fetchWeatherByCoordinates(latitude, longitude) {
     throw error;
   }
 }
-
-// import axios from 'axios';
-
-// const API_KEY = '082d3d02ffdb12f2fd9b259e2ced1d0d';
-// const BASE_URL = 'https://api.openweathermap.org/data/2.5';
-
-// export async function fetchWeatherByCity(city) {
-//   const API_URL = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
-//   try {
-//     const response = await axios.get(API_URL);
-//     return response.data;
-//     // console.log(response.data);
-//   } catch (error) {
-//     console.error('Error fetching weather data:', error);
-//     throw error;
-//   }
-// }
-
-// export async function fetchWeatherByCoordinates(latitude, longitude) {
-//   const API_URL = `${BASE_URL}/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
-//   try {
-//     const response = await axios.get(API_URL);
-//     return response.data;
-//   } catch (error) {
-//     console.error('Error fetching weather data:', error);
-//     throw error;
-//   }
-// }
